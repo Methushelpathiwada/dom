38f9e3add994547925233680011781c175b8104d
Methushelpathiwada <98147356+Methushelpathiwada@users.noreply.github.com>
2022-03-29 12:37:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function click(){
+
+    // document.getElementById("text").innerText="for id"
+
+    //    let output=document.getElementsByClassName("box")
+    //    output[0].innerHTML="for class"
+
+    //    let output=document.getElementsByName("h1")
+    //    output[0].innerHTML="for tags"
+
+    // document.getElementById("hello").innerText="Hello World"
+
+    // document.getElementById("boxes").style.flexDirection="column"
+
+    // document.getElementById("color").style.color="red"
+
+    // document.getElementById('replace').innerText="Welcome to Elevation Academy"
+
+    // let res=document.getElementById("select");
+    // let output=res.value
+    // document.getElementById("print").innerText=output
+
+    
+
+}
